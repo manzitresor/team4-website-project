ce175e5fa47f57728a0678a4af307322d1f762b4
InshutiSouvede <isouvede2@gmail.com>
2024-05-27 16:58:50 +0200

    build: set up the project structure

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        outfit:['Outfit']
+      }
+    },
+  },
+  plugins: [],
+}
+
